6931136b62b83350be33a7301b8e06aff39d9582
Yury Lapshinov <y.raagin@gmail.com>
2020-07-31 00:40:21 +0300

    use reverse for BASE_ADMIN_URL. Solved #13

--- streamfield/settings.py
+++ streamfield/settings.py
@@ -1,6 +1,7 @@
 from django.conf import settings
+from django.urls import reverse_lazy
 
 BLOCK_OPTIONS = getattr(settings, "STREAMFIELD_BLOCK_OPTIONS", {})
 SHOW_ADMIN_HELP_TEXT = getattr(settings, "STREAMFIELD_SHOW_ADMIN_HELP_TEXT", True)
 DELETE_BLOCKS_FROM_DB = getattr(settings, "STREAMFIELD_DELETE_BLOCKS_FROM_DB", True)
-BASE_ADMIN_URL = getattr(settings, "STREAMFIELD_BASE_ADMIN_URL", '/admin/')
+BASE_ADMIN_URL = getattr(settings, "STREAMFIELD_BASE_ADMIN_URL", reverse_lazy('admin:index'))
