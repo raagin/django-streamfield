7c5bec4ef6a1bf17382d7f545028a73cfd8b38a7
Yury Lapshinov <y.raagin@gmail.com>
2021-06-29 11:30:13 +0300

    v1.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-streamfield",
-    version="1.4.0",
+    version="1.4.1",
     author="Yury Lapshinov",
     author_email="y.raagin@gmail.com",
     description="StreamField for native Django Admin or with Grappelli",
--- streamfield/__init__.py
+++ streamfield/__init__.py
@@ -1,2 +1,2 @@
 name = "streamfield"
-VERSION = '1.4.0'
+VERSION = '1.4.1'
