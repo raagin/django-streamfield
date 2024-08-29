9b01c9a10aaee43ff01983201fd597d61262ce13
holasoftware <python3developer@proton.me>
2024-08-29 11:48:21 +0800

    dynamically generated stream texts

---
+++ streamfield/templates/streamfield/streamfield_texts.js
@@ -0,0 +1,13 @@
+{% load i18n %}
+
+window.stream_texts = window.stream_texts || {
+    'deleteBlock': '{% trans "Are you shure, that you want to delete this block?" %}',
+    'deleteInstance': '{% trans "Are you shure, that you want to delete this subblock?" %}',
+    'Collapse all': '{% trans "Collapse all" %}',
+    'Open all': '{% trans "Open all" %}',
+    'AddOneMore': '{% trans "Add one more" %}',
+    'Change': '{% trans "Change" %}',
+    'AddContent': '{% trans "Add content" %}',
+    'Help?': '{% trans "Help?" %}',
+    'AddNewBlock': '{% trans "Add new block" %}'
+}
